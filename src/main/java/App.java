a48ee68afd9cce4a07b58481e506b1cdd7daa473
annaapraxina <91375621+annaapraxina@users.noreply.github.com>
2021-10-14 01:29:22 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        boolean comparison = bean == bean1;
+        boolean comparison1 = bean2 == bean3;
+        System.out.println(comparison);
+        System.out.println(comparison1);
     }
 }
